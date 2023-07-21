2286e5396abdda42d6f929b96682f4d1eaaad59b
takem12 <takemori.job@gmail.com>
2023-07-21 14:19:09 +0000

    add user authorization

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template "users/edit"
     patch user_path(@user), params: { user: { name:  "",
@@ -16,10 +17,13 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "div.alert", "The form contains 4 errors."
   end
 
-  test "successful edit" do
-    det edit_user_path(@user)
-    assert_template "users/edit"
-    name = "Foo Bar"
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_equal edit_user_url(@user), session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
+    name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:   name,
                                               email:  email,
